91d9e47afab9060c42c2c90cdaa8be730628402c
LifeLong-LeArneR17 <118177362+LifeLong-LeArneR17@users.noreply.github.com>
2023-03-24 23:09:55 +0200

    fix
    fix

---
+++ src/FetchPictures.js
@@ -0,0 +1,30 @@
+'use strict'
+export class Api {
+    static BASE_URL = 'https://pixabay.com/api';
+    static API_KEY = '32926626-9f8218f21c9ddc7b36f942801';
+    constructor() {
+        this.page = 1;
+        this.query = null;
+
+    }
+    fetchPhotosbyQuery() {
+        const searchParams = new URLSearchParams({
+            q: 'cat',
+            page: this.page,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: 20,
+        })
+        return fetch(`${Api.BASE_URL}?key=32926626-9f8218f21c9ddc7b36f942801&${searchParams}`).then(response => {
+            if (!response.ok) {
+                throw new Error(response.status)
+            }
+            return response.json()
+        }).then(data => {
+        }).catch(error => {
+        })
+    }
+}
+
+
