5ccd1cec29a050f65b040badff4f6c953aede002
LifeLong-LeArneR17 <118177362+LifeLong-LeArneR17@users.noreply.github.com>
2023-03-24 17:39:18 +0000

    Deploying to gh-pages from @ LifeLong-LeArneR17/hw-11@5d713c1ec09f5b1f517e2e646541e3251016b0fe 🚀

--- index.412de9f8.js
+++
@@ -1,2 +0,0 @@
-fetch("".concat("https://pixabay.com/api","?key=32926626-9f8218f21c9ddc7b36f942801&q=dog&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=5")).then((function(o){if(!o.ok)throw new Error(o.status);return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.log(o)}));
-//# sourceMappingURL=index.412de9f8.js.map
